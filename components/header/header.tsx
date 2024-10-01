85f596373e2f0c42e29b55bf72638c01fee5f0b7
krisna <krisna.w2010@gmail.com>
2024-10-01 11:58:03 +0800

    fix error

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -35,10 +35,10 @@ function Header() {
                         <TopAvatar/>
                     </div>
                     <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                        <NavigationLink href={'/'}>About</NavigationLink>
-                        <NavigationLink href={'/articles'}>Articles</NavigationLink>
-                        <NavigationLink href={'/projects'}>Projects</NavigationLink>
-                        <NavigationLink href={'/tools'}>Tools</NavigationLink>
+                        {/* <NavigationLink href={'/'}>About</NavigationLink> */}
+                        <NavigationLink href={'/'}>Articles</NavigationLink>
+                        {/* <NavigationLink href={'/projects'}>Projects</NavigationLink>
+                        <NavigationLink href={'/tools'}>Tools</NavigationLink> */}
                     </ul>
                     <div>
                         <ThemeToggle/>
